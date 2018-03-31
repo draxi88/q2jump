78e1fd4f562bb506a1982dc3356f8b4e91998cd9
maxwagner <maxwell.v.wagner@gmail.com>
2018-03-31 16:06:48 -0400

    old votetime bug from wireplay code

--- src/g_ctf.c
+++ src/g_ctf.c
@@ -3320,7 +3320,7 @@ void CTFWinElection(int pvote, edict_t* pvoter)
 		gi.bprintf(PRINT_HIGH, "%s Adding %i minutes.\n", msg, ctfgame.ekicknum);
 		map_added_time += ctfgame.ekicknum;
 		Update_Added_Time();
-		timeleft = ((int)(level.time / 60)) + (mset_vars->timelimit + map_added_time);
+		timeleft = ((mset_vars->timelimit + map_added_time) - ((int)(level.time / 60))); // old ((int)(level.time / 60)) + (mset_vars->timelimit + map_added_time);
 		ctfgame.election = ELECT_NONE;
 		if (timeleft < 0)
 		{
@@ -3328,6 +3328,10 @@ void CTFWinElection(int pvote, edict_t* pvoter)
 			End_Jumping();
 			return;
 		}
+		if ((map_added_time<0) && (map_added_time<(-timeleft))){
++			End_Jumping();
++			return;
++		}
 		break;
 	case ELECT_SILENCE :
 		ctfgame.ekick->client->resp.silence = true;
