43d61dca8896b61094785fa71b6c5e3ab927655c
draxi <lyntotto@hotmail.com>
2018-08-16 20:25:11 +0200

    if style = 1, the teleporter will teleport any player with a greater checkpointvalue than count. If style is not set, checkpointvalue and count has to match.

--- src/g_misc.c
+++ src/g_misc.c
@@ -1827,8 +1827,14 @@ void teleporter_touch (edict_t *self, edict_t *other, cplane_t *plane, csurface_
 	CTFPlayerResetGrapple(other);
 //ZOID
 
-    if(other->client->pers.checkpoints<self->count){
-        return;
+    if(self->style==1){
+        if(other->client->pers.checkpoints<self->count){
+            return;
+        }
+    } else {
+        if(other->client->pers.checkpoints!=self->count){
+            return;
+        }
     }
 	// unlink to make sure it can't possibly interfere with KillBox
 	gi.unlinkentity (other);
