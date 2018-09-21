6e8f46811d28254929dd597477aec2d62226fedb
Max Wagner <maxwell.v.wagner@gmail.com>
2018-09-21 15:22:25 -0400

    removed a redundant check in fps kicking

--- src/p_client.c
+++ src/p_client.c
@@ -1814,12 +1814,12 @@ void ClientUserinfoChanged (edict_t *ent, char *userinfo)
 	// check for the string, fpskick mset
 	if (strlen(s) && mset_vars->fpskick == 1) {
 		ent->client->pers.fps = atoi(s);
-		if (ent->client->pers.fps>0 && ent->client->pers.fps<20) { // kick for lower than 20
+		if (ent->client->pers.fps<20) { // kick for lower than 20
             gi.cprintf (ent,PRINT_HIGH, "[JumpMod]   You have been kicked for lowering CL_MAXFPS below 20\n");
 			sprintf(temps,"kick %d\n",ent-g_edicts-1);
 			gi.AddCommandString(temps);
 		}
-		if (ent->client->pers.fps>0 && ent->client->pers.fps>120) { // kick for higher than 120
+		if (ent->client->pers.fps>120) { // kick for higher than 120
             gi.cprintf (ent,PRINT_HIGH, "[JumpMod]   You have been kicked for raising CL_MAXFPS above 120\n");
 			sprintf(temps,"kick %d\n",ent-g_edicts-1);
 			gi.AddCommandString(temps);
