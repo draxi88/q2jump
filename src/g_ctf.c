749ba6b089cbcae611aa39bc6459b60c15f23d53
Max Wagner <maxwell.v.wagner@gmail.com>
2018-09-24 13:02:45 -0400

    Revert "ctf tech can never spawn"
    This reverts commit 25cb53f5d821d3fdd64438047eaba25f5dcd204a.

--- src/g_ctf.c
+++ src/g_ctf.c
@@ -2377,7 +2377,7 @@ static void SpawnTechs(edict_t *ent)
 	while (tnames[i]) {
 		if ((tech = FindItemByClassname(tnames[i])) != NULL &&
 			(spot = FindTechSpawn()) != NULL)
-			// SpawnTech(tech, spot); dont spawn tech no matter what
+			SpawnTech(tech, spot);
 		i++;
 	}
 	if (ent)
