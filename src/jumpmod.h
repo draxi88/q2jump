9c839b4d76a066b0b87da2dffbd370facc33ead1
draxi <lyntotto@hotmail.com>
2018-12-04 19:33:07 +0100

    See raceline trough the whole map? not working.. Gotta commit it to work on the scoreboard =(

--- src/jumpmod.h
+++ src/jumpmod.h
@@ -570,6 +570,7 @@ extern char zbbuffer2[256];
 
 void KillMyRox(edict_t *ent);
 void Cmd_Race (edict_t *ent);
+void Cmd_Raceline (edict_t *ent);
 void stuffcmd(edict_t *e, char *s);
 int Q_stricmp (char *s1, char *s2);
 char *Info_ValueForKey (char *s, char *key);
