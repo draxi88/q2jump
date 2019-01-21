2b05a519588d2286c21cd0cdc06caa21f3ff442a
Max Wagner <maxwell.v.wagner@gmail.com>
2019-01-20 23:10:07 -0500

    changelog and version increase

--- src/jumpmod.h
+++ src/jumpmod.h
@@ -3,7 +3,7 @@
 //defines
 #define MAX_USERS 4096
 #define MAX_HIGHSCORES 15
-#define CTF_VERSION_S		"1.26ger"
+#define CTF_VERSION_S		"1.27ger"
 #define		HOOK_READY	0
 #define		HOOK_OUT	1
 #define		HOOK_ON		2
