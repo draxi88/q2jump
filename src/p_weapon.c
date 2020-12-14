ff6138e4ead2f6716d39124c3264a333d039413b
Miikka Ylätalo <totally.mehis@gmail.com>
2020-12-14 16:38:56 +0200

    Fix handheld grenades stopping the player from switching weapons (issue #94)

--- src/p_weapon.c
+++ src/p_weapon.c
@@ -667,7 +667,9 @@ void weapon_grenade_fire (edict_t *ent, qboolean held)
 
 	timer = ent->client->grenade_time - level.time;
 	speed = GRENADE_MINSPEED + (GRENADE_TIMER - timer) * ((GRENADE_MAXSPEED - GRENADE_MINSPEED) / GRENADE_TIMER);
-	fire_grenade2 (ent, start, forward, damage, speed, timer, radius, held);
+
+	if (mset_vars->weapons || mset_vars->rocket)
+		fire_grenade2 (ent, start, forward, damage, speed, timer, radius, held);
 
 	if (!mset_vars->rocket)
 	if (! ( (int)dmflags->value & DF_INFINITE_AMMO ) )
@@ -696,11 +698,11 @@ void weapon_grenade_fire (edict_t *ent, qboolean held)
 
 void Weapon_Grenade (edict_t *ent)
 {
-//	if (((ent->client->newweapon) && (ent->client->weaponstate == WEAPON_READY)) | (!mset_vars->weapons))
-//	{
-//		ChangeWeapon (ent);
-//		return;
-//	}
+	if ((ent->client->newweapon) && (ent->client->weaponstate == WEAPON_READY))
+	{
+		ChangeWeapon (ent);
+		return;
+	}
 
 	if (ent->client->weaponstate == WEAPON_ACTIVATING)
 	{
