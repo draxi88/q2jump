f93f4e391a5d9674eb50fa332c28b490891e063a
draxi88 <64629010+draxi88@users.noreply.github.com>
2023-08-10 23:00:55 +0200

    bool fix?

--- src/q_shared.h
+++ src/q_shared.h
@@ -54,6 +54,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 typedef unsigned char 		byte;
+#define true 1
+#define false 0
 typedef enum {false, true}	qboolean;
 
 
